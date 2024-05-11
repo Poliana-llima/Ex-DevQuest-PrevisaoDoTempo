e336848117ec797ecdb22a226925bef96fcd6fd9
Poliana Lima <109768571+Poliana-llima@users.noreply.github.com>
2024-05-11 18:07:31 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+const chaveDaApi ="c5921751e2eb410db3d201955241105";
+
+const botaoDeBusca = document.querySelector(".btn-busca");
+
+botaoDeBusca.addEventListener("click", async () => {
+    const cidade = document.getElementById("input-busca").value;
+
+    if(!cidade) return;
+
+    const dados = await buscarDadosDaCidade(cidade);
+
+    if (dados) preencherDadoNaTela (dados, cidade);
+
+});
+
+async function buscarDadosDaCidade(cidade) {
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt` ;
+
+    const response = await fetch(apiUrl);
+
+    if(resposta.status !== 200) return;
+
+    const dados = response.json();
+
+    return dados;
+}
+
+function preencherDadoNaTela(dados,cidade) {
+    const temperatura = dados.current.temp_c;
+    const condicao = dados.current.condition.text;
+    const humidade = dados.current.humidity;
+    const velocidadeDoVento = dados.current.wind_kph;
+    const iconeCondicao = dados.current.condition.icon;
+
+    document.getElementById("cidade").textContent = cidade;
+
+    document.getElementById("temperatura").textContent = `${temperatura}°C`;
+
+    document.getElementById("condicao").textContent = condicao;
+
+    document.getElementById("humidade").textContent = `${humidade}%`;
+
+    document.getElementById("velocidade-do-vento").textContent = `${velocidadeDoVento}km/h`;
+
+    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
+
+}
